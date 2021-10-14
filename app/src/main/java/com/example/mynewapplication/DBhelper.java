b33bbe9f10330fc7ddb6c5aec44d22b595cb9e93
Rav3nFK <shabanin.serzh@mail.ru>
2021-10-14 03:23:24 +0300

    Переработка смысла справочника (справочник книг):  -Изменения интерфейса  -Изменение основного кода  -Изменения класса DBhelper

--- app/src/main/java/com/example/mynewapplication/DBhelper.java
+++ app/src/main/java/com/example/mynewapplication/DBhelper.java
@@ -7,15 +7,18 @@ import android.os.Build;
 
 import androidx.annotation.Nullable;
 
+
 public class DBhelper extends SQLiteOpenHelper {
 
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
     public static final String DATABASE_NAME = "contactDb";
     public static final String TABLE_CONTACTS = "contacts";
 
     public static final String KEY_ID = "_id";
     public static final String KEY_NAME = "name";
-    public static final String KEY_MAIL = "mail";
+    public static final String KEY_AUTHOR = "author";
+    public static final String KEY_STYLE = "style";
+    public static final String KEY_COST = "cost";
 
     public DBhelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -24,7 +27,7 @@ public class DBhelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table " + TABLE_CONTACTS + "(" + KEY_ID
-                + " integer primary key," + KEY_NAME + " text," + KEY_MAIL + " text" + ")");
+                + " integer primary key," + KEY_NAME + " text," + KEY_AUTHOR + " text," + KEY_STYLE +" text," + KEY_COST + " integer" + ")");
     }
 
     @Override
--- app/src/main/java/com/example/mynewapplication/MainActivity.java
+++ app/src/main/java/com/example/mynewapplication/MainActivity.java
@@ -8,16 +8,21 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button readBt, addBt, clearBt;
-    EditText NameET, MailET;
-    TextView Out;
+    Button addBt, clearBt;
+    EditText NameET, AuthorET, StyleET, CostET;
     DBhelper dBhelper;
+    SQLiteDatabase database;
+    ContentValues contentValues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,71 +30,160 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         addBt = (Button) findViewById(R.id.AddBut);
-        readBt = (Button) findViewById(R.id.ReadBut);
-        clearBt = (Button) findViewById(R.id.ClearBut);
 
+        clearBt = (Button) findViewById(R.id.ClearBut);
         addBt.setOnClickListener(this);
-        readBt.setOnClickListener(this);
         clearBt.setOnClickListener(this);
 
         NameET = (EditText) findViewById(R.id.NameET);
-        MailET = (EditText) findViewById(R.id.MailET);
-        Out = (TextView) findViewById(R.id.Out);
+        AuthorET = (EditText) findViewById(R.id.Author);
+        StyleET = (EditText) findViewById(R.id.Style);
+        CostET = (EditText) findViewById(R.id.Cost);
 
         dBhelper = new DBhelper(this);
+        database = dBhelper.getWritableDatabase();
+        UpdateTable();
+
+    }
+
+    public void UpdateTable() {
+        Cursor cursor = database.query(DBhelper.TABLE_CONTACTS, null, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            int idIndex = cursor.getColumnIndex(DBhelper.KEY_ID);
+            int nameIndex = cursor.getColumnIndex(DBhelper.KEY_NAME);
+            int authIndex = cursor.getColumnIndex(DBhelper.KEY_AUTHOR);
+            int styleIndex = cursor.getColumnIndex(DBhelper.KEY_STYLE);
+            int costIndex = cursor.getColumnIndex(DBhelper.KEY_COST);
+
+            TableLayout layOutput = findViewById(R.id.TabLay);
+            layOutput.removeAllViews();
+            do {
+                TableRow TBrow = new TableRow(this);
+                TBrow.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+                TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+
+                TextView outputID = new TextView(this);
+                params.weight = 1.0f;
+                outputID.setLayoutParams(params);
+                outputID.setText(cursor.getString(idIndex));
+                TBrow.addView(outputID);
+
+                TextView outputNm = new TextView(this);
+                params.weight = 2.0f;
+                outputNm.setLayoutParams(params);
+                outputNm.setText(cursor.getString(nameIndex));
+                TBrow.addView(outputNm);
+
+                TextView outputAuth = new TextView(this);
+                params.weight = 2.0f;
+                outputAuth.setLayoutParams(params);
+                outputAuth.setText(cursor.getString(authIndex));
+                TBrow.addView(outputAuth);
+
+                TextView outStyle = new TextView(this);
+                params.weight = 2.0f;
+                outStyle.setLayoutParams(params);
+                outStyle.setText(cursor.getString(styleIndex));
+                TBrow.addView(outStyle);
+
+                TextView outCost = new TextView(this);
+                params.weight = 1.0f;
+                outCost.setLayoutParams(params);
+                outCost.setText(cursor.getString(costIndex));
+                TBrow.addView(outCost);
 
 
+
+
+                Button clearOnEl = new Button(this);
+                clearOnEl.setOnClickListener(this);
+                params.weight = 1.0f;
+                clearOnEl.setLayoutParams(params);
+                clearOnEl.setText("Удоли");
+                clearOnEl.setId(cursor.getInt(idIndex));
+                TBrow.addView(clearOnEl);
+
+                layOutput.addView(TBrow);
+
+
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
     }
 
     @Override
     public void onClick(View v) {
-        String name = NameET.getText().toString();
-        String mail = MailET.getText().toString();
 
-        SQLiteDatabase database = dBhelper.getWritableDatabase();
-        ContentValues contentValues = new ContentValues();
 
         switch (v.getId()) {
             case R.id.AddBut:
-                if (name.isEmpty() && mail.isEmpty()) {
+                String name = NameET.getText().toString();
+                String auth = AuthorET.getText().toString();
+                String style = StyleET.getText().toString();
+                String cost = CostET.getText().toString();
+
+
+                contentValues = new ContentValues();
+                if (name.isEmpty()) {
                     Toast toast = Toast.makeText(getApplicationContext(),
-                    "Поля пусты...", Toast.LENGTH_SHORT);
+                            "Необходимо название книги...", Toast.LENGTH_SHORT);
                     toast.show();
                 } else {
                     contentValues.put(DBhelper.KEY_NAME, name);
-                    contentValues.put(DBhelper.KEY_MAIL, mail);
+                    contentValues.put(DBhelper.KEY_AUTHOR, auth);
+                    contentValues.put(DBhelper.KEY_STYLE, style);
+                    contentValues.put(DBhelper.KEY_COST, cost);
 
                     database.insert(DBhelper.TABLE_CONTACTS, null, contentValues);
                 }
-                break;
-            case R.id.ReadBut:
-                Cursor cursor = database.query(DBhelper.TABLE_CONTACTS, null, null, null, null, null, null);
-                if (cursor.moveToFirst()) {
-                    int idIndex = cursor.getColumnIndex(DBhelper.KEY_ID);
-                    int nameIndex = cursor.getColumnIndex(DBhelper.KEY_NAME);
-                    int mailIndex = cursor.getColumnIndex(DBhelper.KEY_MAIL);
-                    Out.setText(null);
-                    do {
-                        Out.append("ID = " + cursor.getInt(idIndex) +
-                                ", name = " + cursor.getString(nameIndex) +
-                                ", email = " + cursor.getString(mailIndex) + "\n");
 
-                        Log.d("mLog", "ID = " + cursor.getInt(idIndex) +
-                                ", name = " + cursor.getString(nameIndex) +
-                                ", email = " + cursor.getString(mailIndex));
-                    }
-                    while (cursor.moveToNext());
-                } else {
-                    Log.d("mLog", "0 rows");
-                    Out.setText("Записей нет");
-                }
-                cursor.close();
+                UpdateTable();
+                NameET.setText(null);
+                AuthorET.setText(null);
+                StyleET.setText(null);
+                CostET.setText(null);
                 break;
+
+
             case R.id.ClearBut:
                 database.delete(DBhelper.TABLE_CONTACTS, null, null);
+                TableLayout layOutput = findViewById(R.id.TabLay);
+                layOutput.removeAllViews();
+                break;
+            default:
+                View outputDBRow = (View) v.getParent();
+                ViewGroup outputDB = (ViewGroup) outputDBRow.getParent();
+                outputDB.removeView(outputDBRow);
+                outputDB.invalidate();
+
+                database.delete(DBhelper.TABLE_CONTACTS, DBhelper.KEY_ID + " = ?", new String[]{String.valueOf((v.getId()))});
+                contentValues = new ContentValues();
+                Cursor cursorupd = database.query(DBhelper.TABLE_CONTACTS, null, null, null, null, null, null);
+                if (cursorupd.moveToFirst()) {
+                    int idIndex = cursorupd.getColumnIndex(DBhelper.KEY_ID);
+                    int nameIndex = cursorupd.getColumnIndex(DBhelper.KEY_NAME);
+                    int authIndex = cursorupd.getColumnIndex(DBhelper.KEY_AUTHOR);
+                    int styleIndex = cursorupd.getColumnIndex(DBhelper.KEY_STYLE);
+                    int costIndex = cursorupd.getColumnIndex(DBhelper.KEY_COST);
+                    int realId = 1;
+                    do {
+                        if (cursorupd.getInt(idIndex) > realId) {
+                            contentValues.put(dBhelper.KEY_ID, realId);
+                            contentValues.put(DBhelper.KEY_NAME, cursorupd.getString(nameIndex));
+                            contentValues.put(DBhelper.KEY_AUTHOR, cursorupd.getString(authIndex));
+                            contentValues.put(DBhelper.KEY_STYLE, cursorupd.getString((styleIndex)));
+                            contentValues.put(DBhelper.KEY_COST, cursorupd.getString((costIndex)));
+                            database.replace(DBhelper.TABLE_CONTACTS, null, contentValues);
+                        }
+                        realId++;
+                    } while (cursorupd.moveToNext());
+                    if (cursorupd.moveToLast() && v.getId()!=realId) {
+                        database.delete(DBhelper.TABLE_CONTACTS, DBhelper.KEY_ID + " = ?", new String[]{cursorupd.getString(idIndex)});
+                    }
+                }
                 break;
         }
-        dBhelper.close();
     }
 
 
